f72f4f70ffd8fcdd784c9c899181382cf1a91317
Alyssa Geria <alyssageria@gmail.com>
2023-04-15 08:15:42 -0700

    updated index.js and created a functions.js file

---
+++ db/functions.js
@@ -0,0 +1,35 @@
+const inquirer = require('inquirer');
+const mainIndex = require('../index');
+const index = require('index.js')
+
+function viewAllDepartments() {
+
+}
+
+function viewAllRoles() {
+
+}
+
+function viewAllEmployees() {
+
+}
+
+function addDepartment() {
+
+}
+
+function addRole() {
+
+}
+
+function addEmployee() {
+
+}
+
+function updateEmployeeRole() {
+
+}
+
+function quit() {
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const db = require('./db')
 const consoleTable = require('console.table');
+const functions = require('./db/functions');
 
 function loadPrompts() {
     inquirer.prompt([
@@ -11,41 +12,71 @@ function loadPrompts() {
             choices: [
                 {
                     name: "View All Departments",
-                    value: "view_all_departments"
+                    value: "VIEW_ALL_DEPARTMENTS"
 
                 },
                 {
                     name: "View All Roles",
-                    value: "all_roles"
+                    value: "ALL_ROLES"
 
                 },
                 {
                     name: "View All Employees",
-                    value: "view_employees"
+                    value: "VIEW_EMPLOYEES"
 
                 },
                 {
                     name: "Add a Department",
-                    value: "add_department"
+                    value: "ADD_DEPARTMENT"
 
                 },
                 {
                     name: "Add a Role",
-                    value: "add_role"
+                    value: "ADD_ROLE"
                 },
                 {
                     name: "Add an Employee",
-                    value: "add_employee"
+                    value: "ADD_EMPLOYEE"
                 },
                 {
                     name: "Update an Employee Role",
-                    value: "update_employee_role"
+                    value: "UPDATE_EMPLOYEE_ROLE"
                 },
                 {
                     name: "Quit",
-                    value: "quit"
+                    value: "QUIT"
                 }
             ]
         }
-    ])
+    ]).then(res => {
+        let choice = res.choice;
+
+        switch (choice) {
+            case "VIEW_ALL_DEPARTMENTS":
+                viewAllDepartments();
+                break;
+            case "ALL_ROLES":
+                viewAllRoles();
+                break;
+            case "VIEW_EMPLOYEES":
+                viewAllEmployees();
+                break;
+            case "ADD_DEPARTMENT":
+                addDepartment();
+                break;
+            case "ADD_ROLE":
+                addRole();
+                break;
+            case "ADD_EMPLOYEE":
+                addEmployee();
+                break;
+            case "UPDATE_EMPLOYEE_ROLE":
+                updateEmployeeRole();
+                break;
+            case 'QUIT':
+                quit();
+                break;
+        }
+    })
 }
+
